82b5df9fe76f32b2037f5b83cd30e8313ee76f4b
darthven <kostharl@gmail.com>
2018-11-28 16:34:42 +0200

    Some updates were made

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api/*': {
-                target: 'http://localhost:3000/api',
+                target: 'http://localhost:3000',
                 secure: false
             }
         }
