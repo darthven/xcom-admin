194d6bd1adf12899ebb87c5bbf920f9351335ca3
darthven <kostharl@gmail.com>
2018-12-03 18:34:25 +0200

    some fixes

--- src/router.js
+++ src/router.js
@@ -21,7 +21,6 @@ const ifAuthenticated = (to, from, next) => {
 
 export default new Router({
     mode: 'history',
-    base: process.env.BASE_URL,
     routes: [
         {
             path: '/',
