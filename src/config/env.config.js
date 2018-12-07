bcc4a190bf1e7016fc3705470f20546c7db260d4
darthven <kostharl@gmail.com>
2018-12-07 18:17:18 +0200

    Some fixes were made

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,3 +1,3 @@
 export const XCOM_USER = process.env.ECOM_USER || 'admin'
 export const XCOM_PASS = process.env.ECOM_PASS || 'admin'
-export const XCOM_URL = process.env.XCOM_URL || 'http://138.68.86.83/api'
+export const XCOM_URL = process.env.XCOM_URL || 'https://app.6030000.ru/api'
